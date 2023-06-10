1cd5bbb7ee1940e84117c9048d132157e6c4be08
MateuszCharysz <charyszek@interia.pl>
2023-06-10 17:44:19 +0200

    logic mess up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form } from './form/form';
 import Input from './input/input';
 import ContactList from './contact-list/contact-list';
@@ -6,86 +6,93 @@ import css from './App.module.css';
 import { nanoid } from 'nanoid';
 import JsLocalStorage from './JsLocalStorage';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  changeHandler = e => {
+  const filterHandler = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    if (name === 'filter') {
+      setFilter(value);
+    }
   };
 
-  filterContacts = () =>
-    this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase()),
+  const filterContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
     );
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts.filter(contact => contact.id !== id)],
-    }));
+  const deleteContact = id => {
+    setContacts([...contacts.filter(contact => contact.id !== id)]);
   };
 
-  isName = (contacts = this.state.contacts) =>
-    contacts.name.toLowerCase() === this.filter.toLowerCase();
-
-  submitForm = callback => {
+  const submitForm = callback => {
     if (
-      this.state.contacts.filter(contact => contact.name === callback.name)
-        .length !== 1
+      contacts.filter(contact => contact.name === callback.name).length !== 1
     ) {
-      let formState = { id: this.idCreate(), ...callback };
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, formState],
-      }));
+      let formState = { id: idCreate(), ...callback };
+      setContacts([...contacts, formState]);
     } else {
       alert(`${callback.name} is already in contacts.`);
     }
   };
 
-  idCreate = () => nanoid();
+  const idCreate = () => nanoid();
 
-  render() {
-    return (
-      <div className={css.container}>
-        <h1>Phonebook</h1>
-        <Form handler={this.submitForm} />
-        <h2>Contacts</h2>
-        <Input
-          label="Find contacts by name"
-          type="text"
-          dataName="filter"
-          validation="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Search is not case sensitive"
-          funcChange={this.changeHandler}
-          stateField={this.state.filter}
-        />
-        <ContactList
-          arr={this.filterContacts()}
-          btnHandler={this.deleteContact}
-        />
-      </div>
-    );
-  }
-  componentDidMount(key = 'contacts') {
-    const lsState = JsLocalStorage.load(key);
-    if (localStorage.getItem(key) !== null) {
-      this.setState(() => ({
-        contacts: [...lsState],
-      }));
-    }
-  }
+  // useEffect((key = 'contacts') => {
+  //   if (localStorage.getItem(key) !== null) {
+  //     const lsState = JsLocalStorage.load(key);
+  //     console.log(lsState);
+  //     console.log(localStorage.getItem(key));
+  //     setContacts([...lsState]);
+  //     console.log('onMOunt'); //TODO do usuniecia
+  //   }
+  // }, []);
 
-  componentDidUpdate({ key = 'contacts' }) {
-    JsLocalStorage.save(key, this.state.contacts);
-  }
-}
+  useEffect(
+    (key = 'contacts') => {
+      // console.log(JSON.stringify(JsLocalStorage.load(key)));
+      // console.log(JSON.stringify(contacts));
+      console.log(JSON.stringify(JsLocalStorage.load(key)) !== JSON.stringify(contacts));
+      if (
+        JSON.stringify(JsLocalStorage.load(key)) !== JSON.stringify(contacts)
+      ) {
+        if (localStorage.getItem(key) !== null) {
+          const lsState = JsLocalStorage.load(key);
+          setContacts([...lsState]);
+          console.log('onupdate'); //TODO do usuniecia
+        } else {
+          JsLocalStorage.save(key, contacts);
+          console.log('onMount'); //TODO do usuniecia
+
+        }
+      }
+    },
+    [contacts],
+  );
+
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <Form handler={submitForm} />
+      <h2>Contacts</h2>
+      <Input
+        label="Find contacts by name"
+        type="text"
+        dataName="filter"
+        validation="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Search is not case sensitive"
+        funcChange={filterHandler}
+        stateField={filter}
+      />
+      <ContactList arr={filterContacts()} btnHandler={deleteContact} />
+    </div>
+  );
+};
 
 export default App;
